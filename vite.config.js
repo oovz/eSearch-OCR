ba6804288182a09d13a961641b23d499db617cbe
Otaro <9078877+oovz@users.noreply.github.com>
2025-06-19 05:51:35 -0500

    chore: fix configs

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,8 @@ export default defineConfig({
             formats: ['es', 'umd', 'iife'],
             fileName: (format) => `eSearchOCR.${format}.js`,
         },
+        rollupOptions: {
+            external: ["onnxruntime-common"]
+        }
     },
 });
